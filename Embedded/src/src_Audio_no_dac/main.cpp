d5595d7374652a52351e2892ba2e72424d694778
zayinconde <zjcmusicdk@gmail.com>
2025-11-20 13:37:22 +0100

    desktop new version of audio code to test on laptop

--- Embedded/src/src_Audio_no_dac/main.cpp
+++ Embedded/src/src_Audio_no_dac/main.cpp
@@ -1,1 +1,13 @@
 #include <Arduino.h>
+#include "AudioTools.h"
+#include "BluetoothA2DPSink.h"
+
+AnalogAudioStream out;
+BluetoothA2DPSink a2dp_sink(out);
+
+void setup() {
+    a2dp_sink.start("MyMusic");
+}
+
+void loop() {
+}
